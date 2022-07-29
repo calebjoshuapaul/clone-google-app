f6ee14902e80bc3d99ff80ae926024371c93365b
Caleb <calebjoshuapaul@gmail.com>
2022-07-30 01:39:50 +0530

    Update styles

--- boogle/src/components/Search.js
+++ boogle/src/components/Search.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import MicIcon from "@mui/icons-material/Mic";
 import Button from "@mui/material/Button";
 import "./Search.css";
 
 function Search() {
+  const [input, setInput] = useState("");
+  const search = (e) => {
+    e.preventDefault();
+    console.log(input);
+  };
   return (
-    <div className="search">
+    <form className="search">
       <div className="search__input">
         <SearchIcon className="search__inputIcon" />
-        <input />
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
         <MicIcon className="search__micIcon" />
       </div>
 
       <div className="search__buttons">
-        <Button variant="outlined">Google Search</Button>
+        <Button type="submit" onClick={search} variant="outlined">
+          Google Search
+        </Button>
         <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
-    </div>
+    </form>
   );
 }
 
