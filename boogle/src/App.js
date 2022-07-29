a628df7f9a9032c5c9564c60d3987a501bf19c80
Caleb <calebjoshuapaul@gmail.com>
2022-07-29 21:56:31 +0530

    Update

--- boogle/src/App.js
+++ boogle/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from './pages/Home';
-import SearchPage from './pages/SearchPage'
+import SearchPage from './pages/SearchPage';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -9,13 +9,12 @@ function App() {
     <div className="app">
     {/* Welcome to Boogle, a google clone. */}
       <Router>
+
         <Routes>
-          <Route path="/search">
-            <SearchPage />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
+
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/" element={<Home />} />
+
         </Routes>
 
       </Router>
--- boogle/src/pages/Home.js
+++ boogle/src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Home.css';
+import AppsIcon from '@mui/icons-material/Apps';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div className='home'>
       <div className='home__header'>
-        Header Section
         <div className='home__header__left'>
           <Link to='about'>About</Link>
           <Link to='/store'>Store</Link>
@@ -14,6 +15,8 @@ function Home() {
         <div className='home__header__right'>
           <Link to='/gmail'>Gmail</Link>
           <Link to='/images'>Images</Link>
+          <AppsIcon />
+          <AccountCircleIcon />
         </div>
       </div>
       <div className='home__body'>
